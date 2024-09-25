112c7cee226a6538d312344f41e323efc4b27322
Pixelker <65195939+pixelker@users.noreply.github.com>
2024-09-25 13:21:31 +0200

    Update dark-mode-toggle.js

--- dark-mode-toggle.js
+++ dark-mode-toggle.js
@@ -39,12 +39,12 @@ function colorModeToggle() {
   let lightColors = {};
   let darkColors = {};
   cssVariables.split(",").forEach(function (item) {
-    let lightValue = computed.getPropertyValue(`--color--${item}`);
+    let lightValue = computed.getPropertyValue(`--theme--${item}`);
     let darkValue = computed.getPropertyValue(`--dark--${item}`);
     if (lightValue.length) {
       if (!darkValue.length) darkValue = lightValue;
-      lightColors[`--color--${item}`] = lightValue;
-      darkColors[`--color--${item}`] = darkValue;
+      lightColors[`--theme--${item}`] = lightValue;
+      darkColors[`--theme--${item}`] = darkValue;
     }
   });
 
