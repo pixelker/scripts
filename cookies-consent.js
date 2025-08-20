5d678707eea0bfb73674eb5f4d3da06f28fba25b
Pixelker <65195939+pixelker@users.noreply.github.com>
2025-08-20 10:22:54 +0200

    Update cookies-consent.js

--- cookies-consent.js
+++ cookies-consent.js
@@ -1,5 +1,5 @@
 /**
- * Universal Cookie Consent System 4.2
+ * Universal Cookie Consent System 4.3
  * Copyright 2025 Pixelker
  * Released under the MIT License
  * Released on: August 18, 2025
@@ -10,7 +10,7 @@
     
     class UniversalCookieConsent {
         constructor() {
-            this.version = '4.2';
+            this.version = '4.3';
             this.config = {
                 // Endpoint dinámico con fallback inteligente
                 endpoint: this.buildEndpoint(),
